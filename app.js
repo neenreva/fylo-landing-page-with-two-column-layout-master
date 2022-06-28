113751a0e03e0373eacdcd82d6fc0917eabebbfe
Jimmy G <neenreva@gmail.com>
2022-06-28 12:54:50 -0400

    Final Push

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ document.querySelectorAll("button").forEach((btn) => {
   btn.addEventListener("click", function (e) {
     e.preventDefault();
     if (e.target.previousElementSibling.value.match(/^\S+@\S+\.\S+$/m)) {
-      e.target.nextElementSibling.classList.add("hidden");
+      e.target.parentElement.nextElementSibling.classList.add("hidden");
       e.target.previousElementSibling.style.border = "1px solid";
     } else {
       e.target.previousElementSibling.style.border = "1px solid red";
-      e.target.nextElementSibling.classList.remove("hidden");
+      e.target.parentElement.nextElementSibling.classList.remove("hidden");
     }
   });
 });
